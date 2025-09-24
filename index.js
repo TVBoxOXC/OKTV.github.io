3844a490e0c91f9cd2e587274d4d44a73130fbae
TVBoxOXC <110074820+TVBoxOXC@users.noreply.github.com>
2025-09-24 14:24:56 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css">
+</head>
+
+
+</html>
